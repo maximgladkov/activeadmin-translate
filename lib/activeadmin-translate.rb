b91d2cbbb3d6f64dafbf3b7f33d32ce1bccf7859
Maxim Gladkov <contact@maximgladkov.com>
2014-05-14 19:40:11 +0300

    add custom form builder methods to Formtastic

--- lib/activeadmin-translate.rb
+++ lib/activeadmin-translate.rb
@@ -7,3 +7,4 @@ require 'active_admin/translate/form_builder'
 require 'active_admin/views/translate_attributes_table'
 
 ActiveAdmin::FormBuilder.send(:include, ::ActiveAdmin::Translate::FormBuilder)
+Formtastic::FormBuilder.send(:include, ::ActiveAdmin::Translate::FormBuilder)
